b74fcdc163d3892fef523c3064bd89d4ce96fa64
August Pemberton <augustmpemberton@gmail.com>
2024-05-03 10:25:00 +0100

    pass key equivalent on

--- gui/im_MacOS_Webview.mm
+++ gui/im_MacOS_Webview.mm
@@ -85,6 +85,10 @@
 
 - (BOOL)performKeyEquivalent:(NSEvent *)event
 {
+    if (!acceptKeyEvents) {
+        return NO;
+    }
+
     NSString *characters = [[event charactersIgnoringModifiers] lowercaseString];
     NSEventModifierFlags modifiers = [event modifierFlags];
 
